aaede72b9a5870d875ed64841eed3b60d853767e
Desplay Shido <101574256+Desplay@users.noreply.github.com>
2023-05-21 14:32:20 +0700

    Delete create-tree.js

--- create-tree.js
+++
@@ -1,8 +0,0 @@
-const tree = require('tree-node-cli');
-
-const string = tree('./', {
-    allFiles: true,
-    exclude: [/Static/, /__pycache__/, /node_modules/, /.git/, /.vscode/],
-  });
-  
-console.log(string);
